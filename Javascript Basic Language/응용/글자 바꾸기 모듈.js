a65cd53770817b98fe30b7c3384d1c71335f8d3f
sigterions <pterosaurs__@naver.com>
2022-03-30 19:24:23 +0900

    Update 글자 바꾸기 모듈.js

--- Javascript Basic Language/응용/글자 바꾸기 모듈.js
+++ Javascript Basic Language/응용/글자 바꾸기 모듈.js
@@ -1,3 +1,5 @@
+// getElementByID 값을 반환한다. 
+// textContent는 모든 텍스트를 그대로 가져오지만 innerText는 공백을 제거하고 텍스트를 반환
 var mid = document.querySelector('#mid');
 mid.onclick = function(){
     document.getElementById("mid").textContent = "럭스";
