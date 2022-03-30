4a65c895630c60f61a2ddf05c4af4e9c1f233559
sigterions <pterosaurs__@naver.com>
2022-03-30 19:19:12 +0900

    Change letters

---
+++ Javascript Basic Language/응용/글자 바꾸기 모듈.js
@@ -0,0 +1,9 @@
+var mid = document.querySelector('#mid');
+mid.onclick = function(){
+    document.getElementById("mid").textContent = "럭스";
+}
+
+var top = document.querySelector('#top');
+top.onclick = function(){
+    document.getElementById('top').textContent = "문도";
+}
